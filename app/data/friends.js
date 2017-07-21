ef94092e971829c9ee5b5d66498f23f825c953ce
Alexander Himchak <alexhimchak@alexanders-mbp.attlocal.net>
2017-07-20 22:46:05 -0700

    Works

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friends = [
+  {
+    name: "DonkeyKong",
+    photo: "https://vignette4.wikia.nocookie.net/nintendo/images/9/90/SSB4_-_Donkey_Kong_Artwork.png/revision/latest?cb=20131230011605&path-prefix=en",
+    selection: [
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5,
+    5]
+    
+  }
+];
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friends;
